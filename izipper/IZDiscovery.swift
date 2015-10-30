3ded6c41735e8fefbb6ee19c441789512f2dd46b
Yuan Shen <yshen47@illinois.edu>
2015-10-29 20:03:02 -0500

    added IZDiscovery

---
+++ izipper/IZDiscovery.swift
@@ -0,0 +1,13 @@
+//
+//  IZDiscovery.swift
+//  izipper
+//
+//  Created by Frank on 10/29/15.
+//  Copyright © 2015 izipperteam. All rights reserved.
+//
+
+import Cocoa
+
+class IZDiscovery: NSObject {
+
+}
