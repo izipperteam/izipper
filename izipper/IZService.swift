1a2cb3298d0a15e219eb4602daae9cc7b1bf1b73
Alan Jin <xilunjin7@gmail.com>
2015-10-29 19:57:50 -0500

    added discovery and service classes

---
+++ izipper/IZService.swift
@@ -0,0 +1,13 @@
+//
+//  IZService.swift
+//  izipper
+//
+//  Created by Alan Jin on 10/29/15.
+//  Copyright © 2015 izipperteam. All rights reserved.
+//
+
+import UIKit
+
+class IZService: NSObject {
+
+}
