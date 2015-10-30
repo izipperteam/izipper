4f3c83f5c1bfc8c0808837f8deffa5d234aea026
Richard Wei <xinranmsn@gmail.com>
2015-10-29 20:22:01 -0500

    built up class for IZSerivce

--- izipper/IZService.swift
+++ izipper/IZService.swift
@@ -7,7 +7,59 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class IZService: NSObject {
+
+var kBLEServiceChangedStatusNotification = "kBLEServiceChangedStatusNotification"
+
+class IZService: NSObject, CBPeripheralDelegate {
+    
+    private var peripheral: CBPeripheral
+    private var characteristic: CBCharacteristic
+    
+    init(peripheral: CBPeripheral) {
+        self.peripheral = peripheral
+        self.peripheral.delegate = self
+    }
+    
+    func reset() {
+        self.peripheral = nil
+    
+        
+    }
+    
+    func startDiscoveringServices() {
+        
+        
+    }
+    
+    func writePosition(position: Uint8) {
+        
+        
+    }
+    
+    // CBPeripheralDelegate methods
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        var services = peripheral.services
+        
+        guard let _ = services else {
+            return
+        }
+        
+        if services.count == 0 {
+            return
+        }
+        
+        for service in services {
+            
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        
+    }
+    
+    
 
 }
